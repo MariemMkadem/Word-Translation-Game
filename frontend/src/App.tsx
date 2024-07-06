bbf587658a22fb3c12f3ecc84e510fe2639a9571
MariemMkadem <mmkadem11@gmail.com>
2024-07-06 03:21:26 +0200

    Set up main application file and entry point

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Home} from './pages/home';
+import {Game} from './components/game-input';
+import {Header} from './components/header';
+import { GameProvider } from './context/GameContext';
 
-import "./App.css";
-
-function App() {
+const App = () => {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <GameProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </Router>
+    </GameProvider>
   );
-}
+};
 
 export default App;
